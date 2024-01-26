8b26dee4d4e831375aab1fcd2dc9ace61ba5f3f7
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2024-01-26 23:28:52 +0100

    Update fyShuffle.ts
    converted fyShuffle.ts to pure function

--- lib/utils/fyShuffle.ts
+++ lib/utils/fyShuffle.ts
@@ -1,13 +1,18 @@
-/** A JavaScript implementation of the [Fisher-Yates shuffle](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle) for generating a truly random, unbiased permutation of a finite sequence. */
-export function fyShuffle(elementSet: unknown[]): unknown[] {
-  const randomizedSet = elementSet;
+/**
+ * A JavaScript implementation of the [Fisher-Yates shuffle](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle) for generating a truly random, unbiased permutation of a finite sequence with a pure function.
+ * @author Dominik Bošnjak
+ * @version 0.3
+ */
+export function fyShuffle(array: unknown[]): unknown[] {
 
-  let n = elementSet.length, i;
+    const shuffledArray = Array.from(array);
 
-  while (n > 0) {
-    i = Math.floor(Math.random() * n--);
-    randomizedSet.push(elementSet.splice(i, 1)[0]);
-  };
+    let remainingCount = shuffledArray.length;
 
-  return randomizedSet;
+    while (remainingCount > 0) {
+        const i = Math.floor(Math.random() * remainingCount--);
+        shuffledArray.push(shuffledArray.splice(i, 1)[0]);
+    };
+
+    return shuffledArray;
 };
