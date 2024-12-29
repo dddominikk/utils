62f9f5701b70bd2489c0f1fd15eff7edf4832af0
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2024-12-30 00:19:59 +0100

    Create KeysMatchingValueTypes.ts

---
+++ lib/typescript/KeysMatchingValueTypes.ts
@@ -0,0 +1,12 @@
+/**
+* @example use
+* ```
+* interface Thing {id: string;name: string;price: number;other: { stuff: boolean };};
+* type thing_string_props = KeysMatchingValueTypes<Thing,string>; // 'id'|'name'
+* const thingStringKey: KeysMatchingValueTypes<Thing, string> = 'price'; // Error: Type '"price"' is not assignable to type 'KeysByValueTypes<Thing, string>'.(2322)
+* const thingNumberKey: KeysMatchingValueTypes<Thing,number> = "price"  // valid
+* ```
+*/
+
+
+export type KeysMatchingValueTypes<Obj, Value> = {[Key in keyof Obj]-?: Obj[Key] extends Value ? Key : never}[keyof Obj];
