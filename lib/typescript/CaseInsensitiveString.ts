2d3f299d9e70d07c0493b9d99f6eb0eb4ec190ca
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2024-12-30 00:19:30 +0100

    Create CaseInsensitiveString.ts

---
+++ lib/typescript/CaseInsensitiveString.ts
@@ -0,0 +1,7 @@
+type CaseInsensitiveString<T extends string> =
+    string extends T ? string :
+    T extends `${infer C1}${infer C2}${infer C3}` ? (
+        `${Uppercase<C1> | Lowercase<C1>}${Uppercase<C2> | Lowercase<C2>}${CaseInsensitiveString<C3>}`
+    ) :
+    T extends `${infer C1}${infer C2}` ? `${Uppercase<C1> | Lowercase<C1>}${CaseInsensitiveString<C2>}` :
+    ``;
