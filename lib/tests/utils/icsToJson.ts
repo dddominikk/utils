173cf39834b9e05bf12767da029bf95a73d62846
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2025-03-06 12:53:18 +0100

    Update icsToJson.ts

--- lib/tests/utils/icsToJson.ts
+++ lib/tests/utils/icsToJson.ts
@@ -1,4 +1,5 @@
 import {icsToJson} from './utils/icsToJson';
+import {sortCalendar} from '../../lib/utils/sortCalendar.mjs'
 
 /** Parse a huge ICS file that is OpenCritic's Calendar. */
 const occP = await fetchFile(
@@ -10,6 +11,17 @@ const occP = await fetchFile(
                 ? res.text().then(text => icsToJson(text, ICAL))
                 : (() => { throw TypeError(`Expected text/calendar, not ${ct}`) })(),
     });
+
+
+/** 
+* @example
+import 
+console.log(sortCalendar({ events: ff, sort: 'end', order: 'asc' }))
+console.log(sortCalendar({ events: ff, sort: 'start', order: 'asc' }))
+console.log(sortCalendar({ events: ff, sort: 'start', order: 'desc' }))
+console.log(sortCalendar({ events: ff, sort: 'end', order: 'desc' }))
+*/
+
 
 export default {
   occpTest: { success: Array.isArray(occP) }
